aedf8fd5ee2f761a99d33b27e0d1a225f3173ffa
soroush4208 <soroush.r80@gmail.com>
2024-05-25 15:16:48 +0330

    ref : refactor code in start quiz component

--- src/components/Home/StartQuiz.tsx
+++ src/components/Home/StartQuiz.tsx
@@ -1,13 +1,23 @@
+import { useContextQuestions } from "../../context/ContextProvider";
+
 function StartQuiz() {
+  const { questionsDispatch } = useContextQuestions();
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col justify-between p-4 items-center h-2/3 bg-slate-700 border rounded-lg w-1/2 background-Quiz font-Quiz">
-        <h1 className="font-bold text-3xl">Quiz</h1>
-        <p className="font-bold text-2xl">welcome to Quiz App</p>
-        <div className="flex flex-col justify-center items-center font-bold text-2xl hover:scale-125 transition-all">
-          <p>Get start</p>
-          <p className="cursor-pointer hover:-rotate-45">🚀</p>
-        </div>
+    <div className="flex flex-col items-center justify-between h-full font-Quiz">
+      <h1 className="font-bold text-3xl">Quiz</h1>
+      <p className="font-bold text-2xl text-center">welcome to Quiz App</p>
+      <div className="flex flex-col justify-center items-center font-bold text-2xl hover:scale-125 transition-all">
+        <p>Get start</p>
+
+        <button
+          onClick={() => {
+            return questionsDispatch({ type: "PAGE", payload: 1 });
+          }}
+          className="cursor-pointer hover:-rotate-45"
+        >
+          🚀
+        </button>
       </div>
     </div>
   );
