c6a1c99b1a6b6a5cd2dafdf72be46f1eeb782e3e
soroush4208 <soroush.r80@gmail.com>
2024-05-25 15:15:48 +0330

    feat : create layout components

---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { IoSunny } from "react-icons/io5";
+
+function Header() {
+  return (
+      <div className="fixed top-2 left-2 z-10 cursor-pointer">
+        <IoSunny />
+      </div>
+  );
+}
+
+export default Header;
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import Header from "./Header/Header";
+
+type Props = {
+  children: ReactNode;
+};
+function Layout({ children }: Props) {
+  return (
+    <div className="theme-body-dark">
+      <Header />
+      <div className="flex justify-center items-center h-screen max-h-full">
+        <div className="fixed p-4 items-center bg-slate-700 border  w-full h-full sm:w-1/2 sm:h-2/3 sm:rounded-lg background-Quiz">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
