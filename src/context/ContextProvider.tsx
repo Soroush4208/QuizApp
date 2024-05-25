3e947d78beaf09b8de8179f8496ccc760e745a72
soroush4208 <soroush.r80@gmail.com>
2024-05-25 18:48:33 +0330

    ref : change types in ContextProvider

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,8 +1,43 @@
 import { ReactNode, createContext, useContext, useReducer } from "react";
 
-const initialState = { arrayQuestions: [], page: 0, index: 0 };
+// تعریف تایپ برای یک سوال
+interface Question {
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
 
-export function reducerQuestions(state: any, action: any) {
+// تعریف تایپ برای وضعیت مقدماتی
+interface InitialStateType {
+  arrayQuestions: Question[];
+  page: number;
+  index: number;
+}
+
+// تعریف تایپ برای اکشن‌های ریدیوسر
+type ActionType =
+  | { type: "QUESTION"; payload: Question[] }
+  | { type: "PAGE"; payload: number }
+  | { type: "QUESTION_INDEX" };
+
+// تعریف تایپ برای تابع ریدیوسر
+type ReducerType = (
+  state: InitialStateType,
+  action: ActionType
+) => InitialStateType;
+
+// تعریف محتوای اولیه
+const initialState: InitialStateType = {
+  arrayQuestions: [],
+  page: 0,
+  index: 0,
+};
+
+// تابع ریدیوسر
+function reducerQuestions(
+  state: InitialStateType,
+  action: ActionType
+): InitialStateType {
   switch (action.type) {
     case "QUESTION":
       return { ...state, arrayQuestions: action.payload };
@@ -15,20 +50,28 @@ export function reducerQuestions(state: any, action: any) {
   }
 }
 
-const contextQuestions = createContext({});
+// ایجاد کانتکست
+const contextQuestions = createContext<{
+  questionsState: InitialStateType;
+  questionsDispatch: React.Dispatch<ActionType>;
+}>({ questionsState: initialState, questionsDispatch: () => null });
 
+// هوک مورد استفاده برای دریافت مقادیر کانتکست
 export const useContextQuestions = () => useContext(contextQuestions);
 
-type ContextProviderProps = {
+// تعریف کامپوننت پیوسته
+interface ContextProviderProps {
   children: ReactNode;
-};
+}
 
 function ContextProvider({ children }: ContextProviderProps) {
-
-    const [questionsState ,questionsDispatch]=useReducer(reducerQuestions,initialState)
+  const [questionsState, questionsDispatch] = useReducer<ReducerType>(
+    reducerQuestions,
+    initialState
+  );
 
   return (
-    <contextQuestions.Provider value={{questionsState,questionsDispatch}}>
+    <contextQuestions.Provider value={{ questionsState, questionsDispatch }}>
       {children}
     </contextQuestions.Provider>
   );
