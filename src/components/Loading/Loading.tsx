1968bea0d6d2d4bfb06d89b4cfc8054e866296af
soroush4208 <soroush.r80@gmail.com>
2024-05-25 15:15:05 +0330

    feat : create loading

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+import MAN_ICON from "../../assets/svg/man.svg"
+
+function Loading() {
+  return (
+    <div className="fixed top-[43%] left-[45%]">
+      <div className="relative flex justify-center items-center">
+      <div className="absolute animate-spin rounded-full h-28 w-28 border-t-4 border-b-4 border-red-600"></div>
+      <img
+        src={MAN_ICON}
+        alt="Loading"
+        className="rounded-full h-24 w-24"
+      />
+    </div>
+    </div>
+  );
+}
+
+export default Loading;
