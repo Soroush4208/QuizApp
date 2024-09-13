4357bca19488fa8c42178522ca93f5d10ca1a701
soroush4208 <soroush.r80@gmail.com>
2024-09-13 20:16:01 +0330

    fix : complete project

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,17 @@
 import { IoSunny } from "react-icons/io5";
+import useTheme from "../../hook/useTheme";
 
 function Header() {
+  const { changeMode, mode } = useTheme();
+
+  function toggleMode() {
+    changeMode(mode === "dark" ? "light" : "dark");
+  }
+
   return (
-      <div className="fixed top-2 left-2 z-10 cursor-pointer">
-        <IoSunny />
-      </div>
+    <div className="fixed top-2 left-2 z-10 cursor-pointer">
+      <IoSunny onClick={toggleMode} style={{filter:mode==="dark"?"invert(100%)":"invert(0%)"}}/>
+    </div>
   );
 }
 
